bcde12dbdee4df1479638c8bda2b224af85d0053
Gilson Montanha <gilson.montanha@solvis.com.br>
2021-06-24 14:00:00 -0300

    adapting messages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @msg = "Opa ! Parece que deu boa hein, #{ENV['NOME']}! <br/> Parabéns !"
+    @msg = "Opa ! Parece que deu boa hein, #{ENV['NOME']}! Parabéns !"
   end
 end
